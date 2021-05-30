ee6a150acec7036a14959a08b2207cf27f9da361
moonoonoom <569232693@qq.com>
2021-05-30 17:16:34 +0800

    第一次提交

---
+++ src/views/homepage/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <NavMenu style="position: fixed; width:100%" />
+        <div class="recommend"></div>
+        <div class="recent"></div>
+    </div>
+</template>
+
+<script>
+import NavMenu from '../../components/NavMenu'
+export default {
+  name: 'HomePage',
+  components: {
+    NavMenu
+  }
+}
+</script>
+
+<style scoped>
+.recommend{
+    height: 800px;
+    width: 100%;
+    background: -webkit-linear-gradient(bottom,black,#85C1E9) no-repeat;
+}
+
+.recent{
+    height: 800px;
+    width: 100%;
+    background: -webkit-linear-gradient(left,#85C1E9,#6C3483) no-repeat;
+}
+</style>
